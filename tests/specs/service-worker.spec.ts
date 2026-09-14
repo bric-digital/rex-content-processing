948095d3aa8a79099a1e3bebad31ba700fc7c3bf
Chris Karr <github@audacious-software.com>
2026-09-14 15:04:56 -0500

    CI updates

--- tests/specs/service-worker.spec.ts
+++ tests/specs/service-worker.spec.ts
@@ -7,9 +7,16 @@ test.describe('REX Content Processing', () => {
     test.setTimeout(30_000);
     
     return new Promise<void>((resolve) => {
+      console.log('service worker 1')
+
       setTimeout(() => {
+        console.log('service worker 2')
+
         serviceWorker.evaluate(async () => {
           return new Promise<any>((testResolve) => {
+            console.log('service worker 3')
+
+
             self.rexCorePlugin.handleMessage({
               messageType: 'processContent',
               content: {
@@ -30,11 +37,14 @@ test.describe('REX Content Processing', () => {
                 ],
               }
             }, this, (response:any) => {
+              console.log('service worker 4')
+
               testResolve(response)
             })
           })
         })
         .then((workerResponse) => {
+          console.log('service worker 5')
           expect(workerResponse).toEqual({
             'no_processing': [
               'Service Worker: My phone number is 576-903-8867.',
@@ -52,6 +62,8 @@ test.describe('REX Content Processing', () => {
               'Service Worker: My e-mail address is [EMAIL_*].'
             ]
           })
+
+          console.log('service worker 6')
 
           resolve()
         })
